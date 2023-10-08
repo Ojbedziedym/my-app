b086c760581a054d357598bc7524747714c0c9b5
Adam Doniak <adamdoniak@gmail.com>
2023-10-08 18:28:07 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {default as bemCssModules} from 'bem-css-modules'
+import {default as AppStyles} from'./App.module.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+			<div className="App">
+			
+			</div>
+	);
 }
 
 export default App;
