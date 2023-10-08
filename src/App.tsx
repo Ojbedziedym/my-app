20c58bd90b2fb9ad5d7cc8928aa2c31f52ced3b2
Adam Doniak <adamdoniak@gmail.com>
2023-10-08 18:28:48 +0200

    work

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {default as bemCssModules} from 'bem-css-modules'
 import {default as AppStyles} from'./App.module.css';
+import { Display } from './components/Display/Display';
+import { MemoryContainer } from './containers/MemoryContainer';
+import { MainKeyboard } from './containers/MainKeyboard';
 
+const style = bemCssModules(AppStyles);
 
 function App() {
 	return (
-			<div className="App">
-			
+			<div className={style()}>
+			<Display />
+			<MemoryContainer />
+			<MainKeyboard />
 			</div>
 	);
 }
---
+++ src/containers/MainKeyboard.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {default as bemCssModules} from 'bem-css-modules';
+import {default as MainKeyboardStyles} from './MainKeyboard.module.scss'
+import {Button} from '../components/Button/Button'
+const style =bemCssModules(MainKeyboardStyles);
+
+export const MainKeyboard: React.FC = () => {
+    return (
+        <div className={style()}>
+
+        <Button content="%" onClick={()=> console.log("")} />
+        <Button content="CE" onClick={()=> console.log("")} />
+        <Button content="C" onClick={()=> console.log("")} />
+        <Button content="<=" onClick={()=> console.log("")} />
+        <Button content="1/x" onClick={()=> console.log("")} />
+        <Button content="x2" onClick={()=> console.log("")} />
+        <Button content="sqrt" onClick={()=> console.log("")} />
+        <Button content="/" onClick={()=> console.log("")} />
+        <Button content="7" onClick={()=> console.log("")} />
+        <Button content="8" onClick={()=> console.log("")} />
+        <Button content="9" onClick={()=> console.log("")} />
+        <Button content="*" onClick={()=> console.log("")} />
+        <Button content="4" onClick={()=> console.log("")} />
+        <Button content="5" onClick={()=> console.log("")} />
+        <Button content="6" onClick={()=> console.log("")} />
+        <Button content="-" onClick={()=> console.log("")} />
+        <Button content="1" onClick={()=> console.log("")} />
+        <Button content="2" onClick={()=> console.log("")} />
+        <Button content="3" onClick={()=> console.log("")} />
+        <Button content="+" onClick={()=> console.log("")} />
+        <Button content="+/-" onClick={()=> console.log("")} />
+        <Button content="0" onClick={()=> console.log("")} />
+        <Button content="." onClick={()=> console.log("")} />
+        <Button content="=" onClick={()=> console.log("")} />
+
+            </div>
+    )    
+        }
+   
